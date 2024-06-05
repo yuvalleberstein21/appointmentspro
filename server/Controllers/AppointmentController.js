5c46b2b684f5085ddf15bb3fd325f6502e3b50c7
MailBIT Sales <yuvalleberstein21@gmail.com>
2024-06-05 16:02:28 +0300

    create appointment server

--- server/Controllers/AppointmentController.js
+++ server/Controllers/AppointmentController.js
@@ -5,43 +5,44 @@ const User = require("../models/userModel");
 
 const createAppointment = async (req, res) => {
     try {
-        const userId = req.user._id
+        const userId = req.user._id;
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
 
-        const { businessId, serviceId, appointmentDate, appointmentTime } = req.body;
+        const { business, service, appointmentDate, appointmentTime } = req.body;
 
+        // Log the contents of req.body for debugging
+        console.log("req.body:", req.body);
+        console.log("user._id:", userId);
+        console.log("business:", business);
+        console.log("service:", service);
+        console.log("appointmentDate:", appointmentDate);
+        console.log("appointmentTime:", appointmentTime);
 
-        console.log(user._id);
-        console.log(businessId);
-        console.log(serviceId);
-        console.log(appointmentDate);
-        console.log(appointmentTime);
         // Validate input data
-        if (!businessId || !serviceId || !appointmentDate || !appointmentTime) {
+        if (!business || !service || !appointmentDate || !appointmentTime) {
             return res.status(400).json({ error: 'All fields are required' });
         }
 
-
         // Check if the business exists
-        const business = await Business.findById(businessId);
-        if (!business) {
+        const businessEntity = await Business.findById(business);
+        if (!businessEntity) {
             return res.status(404).json({ error: 'Business not found' });
         }
 
         // Check if the service exists
-        const service = await Service.findById(serviceId);
-        if (!service) {
+        const serviceEntity = await Service.findById(service);
+        if (!serviceEntity) {
             return res.status(404).json({ error: 'Service not found' });
         }
 
         // Create the appointment
         const appointment = new Appointment({
             user: userId,
-            business: businessId,
-            service: serviceId,
+            business: business,
+            service: service,
             appointmentDate: new Date(appointmentDate),
             appointmentTime: appointmentTime,
         });
@@ -52,10 +53,9 @@ const createAppointment = async (req, res) => {
         // Return the created appointment
         return res.status(201).json(appointment);
 
-
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ error: 'An error occurred while creating the business' });
+        return res.status(500).json({ error: 'An error occurred while creating the appointment' });
     }
 };
 
