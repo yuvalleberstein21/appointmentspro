324799474512482bb26433778a82035a2fef0c32
MailBIT Sales <yuvalleberstein21@gmail.com>
2024-06-20 18:00:23 +0300

    update mobile responsive

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../Redux/Actions/AuthAction';
+import { useState } from 'react';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const userLogin = useSelector((state: any) => state.userLogin);
   const { userInfo } = userLogin;
 
   const dispatch = useDispatch<any>();
   const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const handleLogout = (e: any) => {
     e.preventDefault;
@@ -19,17 +26,20 @@ const Header = () => {
     }
   };
   return (
-    <nav className="bg-cyan-950 w-full z-20 top-0 start-0">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+    <nav className="bg-cyan-950 w-full z-20 top-0 left-0">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
+        <Link
+          to="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
           <img src="/logo.webp" className="h-8" alt="EasyMarkLogo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+          <span className="self-center lg:text-xl md:text-md text-white font-semibold whitespace-nowrap sm:block">
             EasyMark.
           </span>
-        </a>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        </Link>
+        <div className="flex md:order-2 md:space-x-0 rtl:space-x-reverse">
           <span
-            className="block text-white rounded md:bg-transparent  md:p-1 mr-4"
+            className=" text-white text-md rounded md:bg-transparent md:p-1 mr-4 mt-2 lg:mt-0"
             aria-current="page"
             dir="rtl"
           >
@@ -38,7 +48,7 @@ const Header = () => {
           {userInfo ? (
             <button
               type="button"
-              className="text-white bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 mr-1 text-center hidden sm:block"
               onClick={handleLogout}
             >
               התנתקות
@@ -47,19 +57,18 @@ const Header = () => {
             <Link to={'/'}>
               <button
                 type="button"
-                className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center hidden sm:block"
               >
                 התחברות
               </button>
             </Link>
           )}
-
           <button
-            data-collapse-toggle="navbar-sticky"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-sticky"
-            aria-expanded="false"
+            aria-expanded={isMenuOpen}
+            onClick={toggleMenu}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -80,14 +89,17 @@ const Header = () => {
           </button>
         </div>
         <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          dir="rtl"
+          className={`w-full md:flex md:items-center md:justify-between md:w-auto md:order-1 ${
+            isMenuOpen ? 'block' : 'hidden'
+          }`}
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border  rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  ">
+          <ul className="flex lg:gap-5 md:gap-2 flex-col p-4 md:p-0 mt-2 font-medium rounded-lg bg-cyan-950 md:flex-row md:space-x-8 md:mt-0 md:border-0">
             <li>
               <Link
                 to={'/home'}
-                className="block py-2 px-3 text-white rounded md:bg-transparent md:text-emerald-100 md:p-0 md:dark:text-emerald-100"
+                className="block py-2 px-3 text-white rounded md:bg-transparent hover:text-emerald-100 md:p-0"
                 aria-current="page"
               >
                 דף הבית
@@ -97,9 +109,33 @@ const Header = () => {
               <li>
                 <Link
                   to={'/appointmentDashboard'}
-                  className="block py-2 px-3 text-white rounded  md:hover:bg-transparent md:hover:text-emerald-100 md:p-0 md:dark:hover:text-emerald-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-white rounded md:hover:bg-transparent hover:text-emerald-100 md:p-0"
                 >
                   ניהול תורים
+                </Link>
+              </li>
+            )}
+            {/* Elements for small screens */}
+
+            {userInfo ? (
+              <li className="block sm:hidden">
+                <button
+                  type="button"
+                  className="text-white bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+                  onClick={handleLogout}
+                >
+                  התנתקות
+                </button>
+              </li>
+            ) : (
+              <li className="block sm:hidden">
+                <Link to={'/'}>
+                  <button
+                    type="button"
+                    className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+                  >
+                    התחברות
+                  </button>
                 </Link>
               </li>
             )}
