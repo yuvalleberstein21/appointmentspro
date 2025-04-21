df00bb62a1dabaf7272b7d12b62d10c5b34a2263
MailBIT Sales <yuvalleberstein21@gmail.com>
2025-04-21 21:40:18 +0300

    update

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -25,6 +25,7 @@ const userSchema = mongoose.Schema({
 }, {
     timestamps: true
 });
+
 userSchema.methods.matchPassword = async function (enterPassword) {
     return await bcrypt.compare(enterPassword, this.password);
 }
